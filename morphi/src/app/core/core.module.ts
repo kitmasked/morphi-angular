89eaf452a533971923870a1e5f965babb2790731
aaronjones1 <jones.aarondavid@gmail.com>
2022-02-20 17:14:46 -0500

    Added module: Core

--- morphi/src/app/core/core.module.ts
+++ morphi/src/app/core/core.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SideNavComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    SideNavComponent
   ]
 })
 export class CoreModule { }
